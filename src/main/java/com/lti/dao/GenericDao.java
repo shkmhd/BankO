d8f3e902a746bac521bf1ea343c4c62056ec2f86
Siddhant Kumar <siddhant.kumar@lntinfotech.com>
2019-07-15 22:21:51 +0530

    Added forms and generic dao

--- src/main/java/com/lti/dao/GenericDao.java
+++ src/main/java/com/lti/dao/GenericDao.java
@@ -1,5 +1,25 @@
 package com.lti.dao;
 
-public class GenericDao {
+import java.util.List;
 
+public abstract class GenericDao {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Transactional
+	public <E> E addToDataBase(E obj) {
+		
+		return em.merge(obj);
+	}
+	
+	/*
+	 * @Transactional public List <Object> fetchByEmail(String email) {
+	 * 
+	 * //Write the Select Query Here and Return the List }
+	 */
+ 	public <E> List <E> fetchAllDetails(Class <E> c) {
+		
+		return em.createQuery("select o from "+c.getName()+" o").getResultList();
+	}
 }
